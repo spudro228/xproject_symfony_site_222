0063ece7a068661e714b4a17be2d79d9ed172b3e
spudro228 <mompizzapls@mail.ru>
2017-02-06 22:34:44 +0300

    remove User entity into UserBundle

---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by Dmity Smolyakov.
+ * User: spudro228
+ * Date: 25.01.2017
+ * Time: 04:23
+ */
+
+namespace UserBundle\Entity;
+
+use \FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=25, unique=true)
+     */
+    //protected $username;
+
+    /**
+     * @ORM\Column(text="string", length=64)
+     */
+    //protected $password;
+
+    /**
+     * @ORM\Column(type="text", length=60, unique=true)
+     */
+    //protected $email;
+
+    //todo: добавить . Активация аккаунта по почте
+    /**
+     * ORM\Column(name="is_active", type="boolean")
+     */
+    //private $isActive;
+
+    //protected $roles;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
