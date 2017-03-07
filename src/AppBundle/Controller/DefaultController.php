043f614c8a2bce66a18b067607adf21382a06853
spudro228 <mompizzapls@mail.ru>
2017-03-07 03:37:52 +0300

    sdfjsodi

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -11,17 +12,29 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @param Request $request
+     * @return Response
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.root_dir') . '/..') . DIRECTORY_SEPARATOR,
         ]);
     }
 
+    /**
+     *
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
     public function testAction(Request $request)
     {
+        $response = new Response();
+        //$response->headers->setCookie(new Cookie('test','Hello, Cookie!!!'));
+        $response->headers->setCookie(new Cookie('test', 'Hello, Cookie!!!',0, '/', null, false, false));
+        $response->send();
         echo "Test!";
         return new Response();
     }
--- src/PostsBundle/Controller/DefaultController.php
+++ src/PostsBundle/Controller/DefaultController.php
@@ -4,10 +4,11 @@ namespace PostsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction(Request $request)
+    public function indexAction(Request $request, Response $response)
     {
         $token = $this->get('security.firewall.map')->getFirewallConfig($request)->getContext();
         return $this->render('PostsBundle:Default:index.html.twig', [
