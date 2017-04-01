f941325dda60f570e574549ea09d1b5172df56bb
spudro228 <mompizzapls@mail.ru>
2017-04-02 00:18:15 +0300

    Create AdminBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new UserBundle\UserBundle(),
             new FileUploadBundle\FileUploadBundle(),
+            new AdminBundle\AdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
